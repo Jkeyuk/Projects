de11c4f6727801868844c859538b774a2b7cb0dc
Jonathan Keyuk <Jonkero2@gmail.com>
2016-01-28 04:02:39 -0500

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -3,17 +3,19 @@ import java.lang.*;
 import java.io.*;
 
 
-public class ReadFile {
+public class ReadFile{
 
-    public static void main(String[] args) throws IOException {
+     public static void main(String []args) throws IOException
+     {
         String data;
         File myFile = new File("ReadFile.java");
         Scanner scan = new Scanner(myFile);
-
-        while (scan.hasNext()) {
-            data = scan.next();
+        
+        while(scan.hasNextLine())
+        {
+            data = scan.nextLine();
             System.out.println(data);
         }
-
-    }
+        
+     }
 }
