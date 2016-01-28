3103655d4fd763efa9e30a529477e30605f8593c
Jonathan Keyuk <Jonkero2@gmail.com>
2016-01-28 04:54:28 -0500

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -14,14 +14,18 @@ public class ReadFile{
         System.out.print("Enter File Name: ");   //ask for file name
         fileName = userInput.nextLine().trim(); //read user input and trim spaces
         
-        File myFile = new File(fileName); //create file with user inputed name
+        File myFile = new File(fileName); //setup file with user inputed name
         Scanner scan = new Scanner(myFile); //scanner to read file
+        
+        File outputFile = new File("output.txt"); //create outputf file
+        PrintStream print = new PrintStream(outputFile);//connect stream to output file
         
         while(scan.hasNextLine())  //loop while there is something to read
         {
             data = scan.nextLine(); //enter line into data
             System.out.println(data);//print data
+            print.println(data);//print data to output file
         }
-        
+        print.close();
      }
 }
