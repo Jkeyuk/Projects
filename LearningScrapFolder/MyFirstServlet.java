cb95a7360d26b0e56b067d4b079850fa4365ee0b
Jonathan Keyuk <Jonkero2@gmail.com>
2016-02-25 02:08:50 -0500

    Update MyFirstServlet.java

--- LearningScrapFolder/MyFirstServlet.java
+++ LearningScrapFolder/MyFirstServlet.java
@@ -1,6 +1,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,14 +17,22 @@ public class MyFirstServlet extends HttpServlet {
             HttpServletResponse response) throws IOException {
         //set response content type
         response.setContentType("text/html");
-        
+
         //setup output stream
         PrintWriter output = response.getWriter();
-        
+
         //write to output
-        output.println("<p>hi</p>");
+        output.println("<p>hi " + request.getParameter("name") + "</p>");
+
+        //get headers then write them to output
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            output.println("<p>" + headerName + ": " + request.getHeader(headerName) + "</p>");
+        }
     }
-    
+
     @Override
-    public void destroy(){}
+    public void destroy() {
+    }
 }
