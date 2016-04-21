c0a1445819a2fc6f6fd6f30748851993a14dd8e2
Jonathan Keyuk <Jonkero2@gmail.com>
2016-04-21 05:07:42 -0400

    updated main
    updated to reflect newer version which keep proper directory structure

--- LearningProjects/ZipMakerApp/ZipMakerAppMain.java
+++ LearningProjects/ZipMakerApp/ZipMakerAppMain.java
@@ -6,8 +6,10 @@ public class ZipMakerAppMain {
         //create zip maker object and point it to destination
         ZipMaker zipper = new ZipMaker(
                 "C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\test.zip");
-        //add directory or file to zip
-        zipper.zip("C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\Testrectory");
+        //add directory or file to zip, must add directory twice to maintain...
+        //proper directory structure 
+        zipper.zip("C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\Testrectory",
+                "C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\Testrectory");
         //must close stream 
         zipper.closeZipStream();
 
