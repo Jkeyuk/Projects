f77ab0bb8263321cfd9b19f7cdac4650645c7969
Jonathan Keyuk <Jonkero2@gmail.com>
2016-04-26 01:54:44 -0400

    add command line interface

--- LearningProjects/ZipMakerApp/ZipMakerAppMain.java
+++ LearningProjects/ZipMakerApp/ZipMakerAppMain.java
@@ -1,18 +1,44 @@
 package zipmakerapp;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ZipMakerAppMain {
 
     public static void main(String[] args) {
-        //create zip maker object and point it to destination
-        ZipMaker zipper = new ZipMaker(
-                "C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\test.zip");
-        //add directory or file to zip, must add directory twice to maintain...
-        //proper directory structure 
-        zipper.zip("C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\Testrectory",
-                "C:\\Users\\Public\\JavascriptApps\\Temp\\Netbeans\\Testrectory");
-        //must close stream 
-        zipper.closeZipStream();
-
+        askForDirectory();
     }
 
+    public static void askForDirectory() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+        System.out.println("***Enter path where you want to save your zip file...");
+        System.out.println("***must end with zipName.zip where zipname can be any name:");
+        String dest = scan.nextLine();
+        try {
+            askForFileToZip(dest);
+        } catch (FileNotFoundException ex) {
+            System.out.println("That pathname incorrect restart and try again....");
+            System.exit(0);
+        }
+    }
+
+    public static void askForFileToZip(String destination) throws FileNotFoundException {
+        Scanner scan = new Scanner(System.in);
+        ZipMaker zipper = new ZipMaker(destination);
+        System.out.println();
+        System.out.println("****To exit program just type exit otherwise****");
+        while (true) {
+            System.out.println("Enter or drag and drop the file or Directory to zip");
+            String toZip = scan.nextLine();
+            if (toZip.equals("exit") || toZip.equals("EXIT")) {
+                System.out.println("Shutting Down...");
+                break;
+            } else {
+                System.out.println("Now Zipping...");
+                zipper.zip(toZip, toZip);
+                zipper.closeZipStream();
+            }
+        }
+    }
 }
