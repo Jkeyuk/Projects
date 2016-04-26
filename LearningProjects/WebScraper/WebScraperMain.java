0ade56077543e69b48407d3cb5e7303104d5464b
Jonathan Keyuk <Jonkero2@gmail.com>
2016-04-26 02:35:27 -0400

    add command line interface

--- LearningProjects/WebScraper/WebScraperMain.java
+++ LearningProjects/WebScraper/WebScraperMain.java
@@ -1,10 +1,24 @@
 package webscraper;
 
+import java.util.Scanner;
+
 public class WebScraperMain {
 
     public static void main(String[] args) {
-        Scraper scraper = new Scraper("http://www.oracle.com/");
-             
-        scraper.saveLinks();
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+        System.out.println("To Exit just enter exit...otherwise");
+        while (true) {
+            System.out.println("Please enter the website to scrape..do not include http://");
+            String site = scan.nextLine();
+            if (site.equals("exit")) {
+                System.out.println("shutting down program...");
+                break;
+            } else {
+                System.out.println("Scrapping site now...");
+                Scraper scraper = new Scraper("http://" + site);
+                scraper.saveLinks();
+            }
+        }
     }
 }
