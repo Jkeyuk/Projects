8d932698c1c612d0da5cc1f186f00a858639a6d9
Jonathan Keyuk <Jonkero2@gmail.com>
2016-05-02 03:03:25 -0400

    IgnoreCaseSensitive

--- LearningProjects/UnZipApp/UnZipperMain.java
+++ LearningProjects/UnZipApp/UnZipperMain.java
@@ -10,16 +10,16 @@ public class UnZipperMain {
 
     public static void promptUser() {
         Scanner scan = new Scanner(System.in);
-        System.out.println();
+        System.out.println("*************************************************");
         System.out.println("Please enter path to output folder:");
         String outFolder = scan.nextLine();
         UnZipper unzipper = new UnZipper(outFolder);
-        System.out.println();
-        System.out.println("To Exit program just enter exit...otherwise");
         while (true) {
+            System.out.println("*************************************************");
+            System.out.println("To Exit program just enter exit...otherwise");
             System.out.println("Please enter full pathname to zip file to unzip:");
             String fileToUnzip = scan.nextLine();
-            if (fileToUnzip.equals("exit")) {
+            if (fileToUnzip.equalsIgnoreCase("exit")) {
                 System.out.println("Shutting down program...");
                 break;
             } else {
