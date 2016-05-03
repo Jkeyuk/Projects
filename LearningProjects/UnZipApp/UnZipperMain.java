e3399bcc63418f0c1610817a17b4640b2699ee60
Jonathan Keyuk <Jonkero2@gmail.com>
2016-05-03 01:12:35 -0400

    refactoringAndComments

--- LearningProjects/UnZipApp/UnZipperMain.java
+++ LearningProjects/UnZipApp/UnZipperMain.java
@@ -8,17 +8,16 @@ public class UnZipperMain {
         promptUser();
     }
 
+    //prompt user for inputs
     public static void promptUser() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("*************************************************");
-        System.out.println("Please enter path to output folder:");
-        String outFolder = scan.nextLine();
+        //get output folder from user
+        String outFolder = promptForOutputFolder();
+        //create unzipper object with out folder
         UnZipper unzipper = new UnZipper(outFolder);
         while (true) {
-            System.out.println("*************************************************");
-            System.out.println("To Exit program just enter exit...otherwise");
-            System.out.println("Please enter full pathname to zip file to unzip:");
-            String fileToUnzip = scan.nextLine();
+            //get files to unzip from user
+            String fileToUnzip = promptForFileToUnzip();
+            //if input is exit shutdown program, else unzip files or directory
             if (fileToUnzip.equalsIgnoreCase("exit")) {
                 System.out.println("Shutting down program...");
                 break;
@@ -27,5 +26,24 @@ public class UnZipperMain {
                 unzipper.unzip(fileToUnzip);
             }
         }
+    }
+
+    //prompt user for path to output folder
+    private static String promptForOutputFolder() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("*************************************************");
+        System.out.println("Please enter path to output folder:");
+        String outFolder = scan.nextLine();
+        return outFolder;
+    }
+
+    //prompt user for files to unzip
+    private static String promptForFileToUnzip() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("*************************************************");
+        System.out.println("To Exit program just enter exit...otherwise");
+        System.out.println("Please enter full pathname to zip file to unzip:");
+        String fileToUnzip = scan.nextLine();
+        return fileToUnzip;
     }
 }
