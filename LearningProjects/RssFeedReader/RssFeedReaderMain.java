38a88cc0a7e7c94d7a41a08f90080b1881808397
Jonathan Keyuk <Jonkero2@gmail.com>
2016-05-07 06:38:51 -0400

    UpdateErrorHandling

--- LearningProjects/RssFeedReader/RssFeedReaderMain.java
+++ LearningProjects/RssFeedReader/RssFeedReaderMain.java
@@ -1,12 +1,12 @@
 package rssfeedreader;
 
-import java.io.IOException;
 import java.util.Scanner;
-import org.xml.sax.SAXException;
 
 public class RssFeedReaderMain {
 
     public static void main(String[] args) {
+        //rss feed reader object
+        RssFeedReader r = null;
         //sentinel to control flow
         boolean sentinel = true;
         //keeps looping until there are no errors with user input
@@ -14,24 +14,22 @@ public class RssFeedReaderMain {
             //get url of rss feed
             String feedToRead = promptUserForFeed();
             //create rss feed reader with url
-            RssFeedReader r = new RssFeedReader(feedToRead);
-            try {//display feed and turn off loop
-                r.displayFeed();
-                sentinel = false;
-                //prompt user for link they wish to visit
-                promptUserForLinkNum(r);
-            } catch (IOException | SAXException ex) {//if error print error message and re loop              
-                errorM();
-            }
-        }
+            r = new RssFeedReader(feedToRead);
+            //display feed and return false if no user input error  
+            sentinel = r.displayFeed();
+        }//ask user which link they want to go to
+        promptUserForLinkNum(r);
     }
 
     //get url to rss feed from user
     private static String promptUserForFeed() {
         Scanner in = new Scanner(System.in);
-        System.out.println("---------------------------------------");
-        System.out.println("Please enter the RSS feed, example: www.somefeed.com");
-        String feed = in.nextLine();
+        String feed = null;
+        do {//loop until user enters valid input
+            System.out.println("---------------------------------------");
+            System.out.println("Please enter the RSS feed, example: www.somefeed.com");
+            feed = in.nextLine();
+        } while (feed.trim().equals(""));
         return feed;
     }
 
@@ -60,9 +58,4 @@ public class RssFeedReaderMain {
         }
     }
 
-    //error message to display if user input bad value
-    private static void errorM() {
-        System.out.println("-----------------------------------------");
-        System.out.println("There was an error with your input, please try again");
-    }
 }
