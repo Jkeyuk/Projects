02524d814792285113cb0d724335081933baf0ed
Jonathan Keyuk <Jonkero2@gmail.com>
2016-07-01 03:26:21 -0400

    addedUserInterface+Comments
    added user interface and comments

--- LearningProjects/WebServer/WebServerMain.java
+++ LearningProjects/WebServer/WebServerMain.java
@@ -1,10 +1,41 @@
 package webserver;
 
- class WebServerMain {
+/**
+ * This web server serves up files to clients on a web browser
+ */
+import java.io.File;
+import java.util.Scanner;
 
+class WebServerMain {
+
+    /**
+     * Application first prompts user for working directory, then prompts user
+     * for port number. then starts the server with the given directory and
+     * port.
+     *
+     * @param args - not used
+     */
     public static void main(String[] args) {
-        WebServer server = new WebServer("C:\\Users\\jonke_000\\Documents\\GitHub\\Jkeyuk.github.io", 8080);
+        String directory = getWorkingDirectory();
+        int port = getPort();
+        WebServer server = new WebServer(directory, port);
         server.start();
     }
 
+    private static String getWorkingDirectory() {
+        String directory = "";
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("Please enter path to the working directory for the server");
+            directory = scan.nextLine();
+        } while (!new File(directory).isDirectory());
+        return directory;
+    }
+
+    private static int getPort() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the port to start server on");
+        int port = scan.nextInt();
+        return port;
+    }
 }
