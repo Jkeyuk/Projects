8f25b3312c5eeb18dfdd4d1b2116efe2ea542b2c
Jonathan Keyuk <Jonkero2@gmail.com>
2016-11-18 00:56:26 -0500

    Revised

--- JavaProjects/EnglishToPigLatin/EnglishToPigLatin.java
+++ JavaProjects/EnglishToPigLatin/EnglishToPigLatin.java
@@ -1,4 +1,4 @@
-package englishtopiglatin;
+package EnglishToPigLatin;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -6,53 +6,48 @@ import java.util.regex.Pattern;
 
 public class EnglishToPigLatin {
 
-    private final String STRING;
+	private final String STRING;
 
-    public EnglishToPigLatin(String STRING) {
-        this.STRING = STRING;
-    }
+	public EnglishToPigLatin(String STRING) {
+		this.STRING = STRING;
+	}
 
-    public void translate() {
-        String[] arrayOfWords = STRING.split(" ");
-        for (String w : arrayOfWords) {
-            String stringMinusPunct = removePunctuation(w);
-            System.out.print(convertToPigLating(stringMinusPunct) + " ");
-        }
-    }
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the message to translate:");
+		EnglishToPigLatin x = new EnglishToPigLatin(scan.nextLine());
+		System.out.println("The message in pig latin is as follows:");
+		System.out.println(x.translate());
+		scan.close();
+	}
 
-    private String convertToPigLating(String s) {
-        String newPigString;
-        Pattern vowels = Pattern.compile("[aeiouAEIOU]");
-        Matcher m = vowels.matcher(s);
-        if (m.find()) {
-            int vowelIndex = m.start();
-            if (vowelIndex > 0) {
-                String startOfString = s.substring(vowelIndex);
-                String endOfString = s.substring(0, vowelIndex) + "ay";
-                newPigString = startOfString + endOfString;
-                return newPigString;
-            } else {
-                return s + "yay";
-            }
-        } else {
-            return s;
-        }
-    }
+	// returns a string that has been translated to pig Latin.
+	private String translate() {
+		String returnString = "";
+		for (String w : STRING.split(" ")) {
+			returnString += convertToPigLating(removePunctuation(w)) + " ";
+		}
+		return returnString;
+	}
 
-    private String removePunctuation(String s) {
-        Pattern punctuation = Pattern.compile("\\p{Punct}");
-        Matcher m = punctuation.matcher(s);
-        String newString = m.replaceAll(" ");
-        return newString.trim();
-    }
+	// converts a single word to pig Latin. returns string.
+	private String convertToPigLating(String s) {
+		Matcher m =  Pattern.compile("[aeiouAEIOU]").matcher(s);
+		if (m.find()) {
+			if (m.start() > 0) {
+				return s.substring(m.start()) + s.substring(0, m.start()) + "ay";
+			} else {
+				return s + "yay";
+			}
+		} else {
+			return s;
+		}
+	}
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the message to translate:");
-        String input = scan.nextLine();
-        EnglishToPigLatin x = new EnglishToPigLatin(input);
-        System.out.println("The message in pig latin is as follows:");
-        x.translate();
-        System.out.println("");
-    }
+	// removes punctuation from a single word, returns word as string.
+	private String removePunctuation(String s) {
+		Pattern punctuation = Pattern.compile("\\p{Punct}");
+		String newString = punctuation.matcher(s).replaceAll(" ");
+		return newString.trim();
+	}
 }
