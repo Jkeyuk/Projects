246d559bb2f6c7de9d5c099741681c06f871e00f
Jkeyuk <Jkeyuk@github.com>
2023-12-09 10:32:32 -0500

    upadate new chad setup

--- jonk_vim/init.lua
+++ jonk_vim/init.lua
@@ -1,9 +1,14 @@
 print("jonk package loaded")
+
+-- initial configs
 vim.o.number = true
 vim.o.clipboard = "unnamedplus"
+vim.o.relativenumber = true
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Lazy Setup
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -20,8 +25,25 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("jonk_vim/plugin_list", opts)
 
+-- Telescope Setup
+
 local local_built = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', local_built.find_files, {})
 vim.keymap.set('n', '<leader>fg', local_built.live_grep, {})
 vim.keymap.set('n', '<leader>fb', local_built.buffers, {})
 vim.keymap.set('n', '<leader>fh', local_built.help_tags, {})
+
+-- Which Key Setup
+
+local wk = require("which-key")
+wk.register({
+  ["<leader>"] = {
+    f = {
+      name = "+file",
+      f = { "<cmd>Telescope find_files<cr>", "Find File" },
+      g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+      b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+      h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
+    },
+  },
+})
